052df6012a59628b04a1ba4c49a732f695db7ac5
R Banu Prasad <rbanu2907@gmail.com>
2024-06-16 10:56:55 +0530

    space_counter.py

--- Problems/space_counter.py
+++ Problems/space_counter.py
@@ -1,22 +1,15 @@
-# space counter
 # logic 1
-
-# print(input().count(" "))
+ print(input().count(" "))
 
 # logic 2
+ n=input()
+ count=0
+ for i in n:
+     if i==' ':
+         count+=1
+ print(count)
 
-# n=input()
-# count=0
-# for i in n:
-#     if i==' ':
-#         count+=1
-# print(count)
 
-#logic 3
-# n=input()
-# h=n.split()
-# print(len(h)-1)
 
-#logic 4
 
 
